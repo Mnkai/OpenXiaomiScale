6cf3013aa7b17478c380e79b22702d57f4d116e3
Minori Hiraoka <minori@mnetwork.co.kr>
2016-08-20 19:47:43 +0900

    Basic function without UI element

--- app/src/main/java/moe/minori/openxiaomiscale/MainActivity.java
+++ app/src/main/java/moe/minori/openxiaomiscale/MainActivity.java
@@ -1,20 +1,18 @@
 package moe.minori.openxiaomiscale;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
-	private BluetoothAdapter mBluetoothAdapter;
-	private boolean mScanning;
-	private Handler mHandler;
-
-	private static final long SCAN_PERIOD = 10000;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -27,60 +25,56 @@ public class MainActivity extends Activity
 	{
 		super.onResume();
 
+		// Android M Permission check
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+		{
+			Log.d("MainActivity", "Is over Android M");
+			if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+			{
+				Log.d("MainActivity", "Permission not declared, trying to get permission...");
+				androidMPermissionCheck();
+				return;
+			}
+		}
+
 		// When activity is resumed, check for SharedPref for previous scale mac
 		SharedPreferences sharedPreferences = getSharedPreferences("default", MODE_PRIVATE);
 		String scaleMac = sharedPreferences.getString("scaleMac", null);
 
-		if ( scaleMac == null ) // Scale is not registered
+		if (scaleMac == null) // Scale is not registered
 		{
-			//TODO: Start scale register activity. Scale register activity should register scale data in SharedPref.
-		}
-	}
-
-	/**
-	 * Starts and stops BLE scanning from device
-	 *
-	 * @param enable
-	 *               If true, start scanning for 10 seconds
-	 *               If false, stop scanning.
-	 *               Scanning will stop automatically after 10 seconds without explicit function call
-	 */
-	private void startBLEScanning(final boolean enable)
-	{
-		if (enable)
-		{
-			mHandler.postDelayed(new Runnable()
-			{
-				@Override
-				public void run()
-				{
-					mScanning = false;
-					mBluetoothAdapter.stopLeScan(mLeScanCallback);
-				}
-			}, SCAN_PERIOD);
-
-			mScanning = true;
-			mBluetoothAdapter.startLeScan(mLeScanCallback);
+			Log.d("MainActivity", "Scale is not registered");
+			Intent intent = new Intent(this, ScaleRegisterActivity.class);
+			startActivity(intent);
 
 		}
 		else
 		{
-			mScanning = false;
-			mBluetoothAdapter.stopLeScan(mLeScanCallback);
+			// Register GATT listener to receive weight event from scale device
+			Log.d("MainActivity", "Scale registered, start BLE scanning for GATT connection");
+
+			Utils.startStopBLEScanning(this, true);
 		}
-
-
 	}
 
-	/**
-	 * Executed when BLE device is detected
-	 */
-	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback()
+
+	@TargetApi(Build.VERSION_CODES.M)
+	private void androidMPermissionCheck()
 	{
-		@Override
-		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord)
+		final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Permission required");
+		builder.setMessage("Starting from Android M, location permission is required to access BLE scan data from background.");
+		builder.setPositiveButton(android.R.string.ok, null);
+		builder.setOnDismissListener(new DialogInterface.OnDismissListener()
 		{
-			// TODO: Do something with scanned device
-		}
-	};
+			public void onDismiss(DialogInterface dialog)
+			{
+				requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+			}
+
+		});
+		builder.show();
+	}
 }
