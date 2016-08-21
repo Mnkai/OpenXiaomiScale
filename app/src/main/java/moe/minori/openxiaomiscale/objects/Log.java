25af1f5283973be8b5b4f0e00c423b048905ece5
Minori Hiraoka <minori@mnetwork.co.kr>
2016-08-21 13:01:00 +0900

    Silent log when in release build

--- app/src/main/java/moe/minori/openxiaomiscale/objects/Log.java
+++ app/src/main/java/moe/minori/openxiaomiscale/objects/Log.java
@@ -1,12 +1,22 @@
 package moe.minori.openxiaomiscale.objects;
 
+import moe.minori.openxiaomiscale.BuildConfig;
+
 /**
+ * Android Log wrapper
  * Created by minori on 16. 8. 20.
  */
 public class Log
 {
 	public static void d (String name, String content)
 	{
-		android.util.Log.d(name, content);
+		if (BuildConfig.DEBUG)
+		{
+			android.util.Log.d(name, content);
+		}
+		else
+		{
+			// Quieten
+		}
 	}
 }
